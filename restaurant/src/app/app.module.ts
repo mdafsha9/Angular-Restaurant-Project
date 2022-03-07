445703cc759d39967d296de6f7185f14f2576952
mdafsha9 <mdafsha9@gmail.com>
2022-03-07 08:36:52 -0600

    restaurant project and json-server with postman

---
+++ restaurant/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddRestoComponent } from './add-resto/add-resto.component';
+import { ListRestoComponent } from './list-resto/list-resto.component';
+import { LoginRestoComponent } from './login-resto/login-resto.component';
+import { RegisterComponent } from './register/register.component';
+import { UpdateRestoComponent } from './update-resto/update-resto.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddRestoComponent,
+    ListRestoComponent,
+    LoginRestoComponent,
+    RegisterComponent,
+    UpdateRestoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ restaurant/src/app/login-resto/login-resto.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoginRestoComponent } from './login-resto.component';
+
+describe('LoginRestoComponent', () => {
+  let component: LoginRestoComponent;
+  let fixture: ComponentFixture<LoginRestoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LoginRestoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginRestoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
